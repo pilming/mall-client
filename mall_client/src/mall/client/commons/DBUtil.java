e44c01c00177e826c23a507364d1cdd8304d6323
pilming <wgt3@naver.com>
2021-04-12 15:02:17 +0900

    DBUtil.getConnection(), DBUtil.close() 메소드 추가

---
+++ mall_client/src/mall/client/commons/DBUtil.java
@@ -0,0 +1,38 @@
+package mall.client.commons;
+
+import java.sql.*;
+
+public class DBUtil {
+	//1. DB연결
+	public Connection getConnection() {
+		Connection conn = null;
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/mall","root","java1004");
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		return conn;
+	}
+	//2. DB자원(connection , statement, resultSet) 해제
+	public void close(Connection conn, PreparedStatement stmt, ResultSet rs) {
+		try {
+			rs.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			stmt.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			conn.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
